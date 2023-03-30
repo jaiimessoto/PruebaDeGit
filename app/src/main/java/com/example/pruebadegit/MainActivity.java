f16bac20e3c39768174dbc656a6e3e7286c66110
jaiimessoto <129385959+jaiimessoto@users.noreply.github.com>
2023-03-30 15:46:24 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/pruebadegit/MainActivity.java
+++ app/src/main/java/com/example/pruebadegit/MainActivity.java
@@ -10,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // Esto es una actualizacion en GitHub
+        // Esto es una primera actualizacion en GitHub
+        // Esto es una segunda actualizacion en GitHub
     }
 }
